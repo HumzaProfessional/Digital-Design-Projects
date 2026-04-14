6a7c3a7d74152c161ef735ec162918c966db80cf
Humza <142959387+HumzaProfessional@users.noreply.github.com>
2026-04-14 00:02:25 -0400

    Update cpu.h

--- Virtualcpu/cpu.h
+++ Virtualcpu/cpu.h
@@ -7,11 +7,13 @@ typedef struct
 {
     uint8_t A; // register A
     uint8_t B; // Register B
-    uint8_t Z; 
+    uint8_t Z; // Zero flag register
+    uint8_t C;  // Carry flag register
     uint16_t PC; // Progrma counter
-    uint8_t memory[256]; // memory
+    uint8_t memory[256]; // memory array
 } CPU;
 
+// function delcxrations
 void initCPU(CPU *cpu);
 void runCPU(CPU *cpu);
 void printCPU(CPU *cpu);
